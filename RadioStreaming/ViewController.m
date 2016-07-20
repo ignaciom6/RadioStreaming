6234d983d91669f337f439e84d6446d5109966a0
Ignacio Mariani <ignaciom6@hotmail.com>
2016-07-20 20:27:43 -0300

    As a user I want to hear an specific radio station

--- RadioStreaming/ViewController.m
+++ RadioStreaming/ViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "ViewController.h"
+#import <MediaPlayer/MediaPlayer.h>
+#import <AVKit/AVKit.h>
 
 @interface ViewController ()
+
+@property (strong, nonatomic) MPMoviePlayerController * player;
+@property (strong, nonatomic) IBOutlet UIButton *playButton;
+@property (strong, nonatomic) IBOutlet UIButton *stopButton;
+@property (assign, nonatomic) BOOL playing;
 
 @end
 
@@ -17,11 +24,46 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.playing = NO;
+    [self configureButtonsWithStatus:self.playing];
+    
+    NSString *radioURL = @"http://suenas.net:8090/pobrejohnny";
+    
+    self.player = [[MPMoviePlayerController alloc] initWithContentURL:[NSURL URLWithString:radioURL]];
+    self.player.movieSourceType = MPMovieSourceTypeStreaming;
+    self.player.view.hidden = YES;
+    [self.view addSubview:self.player.view];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)playRadio:(id)sender
+{
+    [self.player prepareToPlay];
+    [self.player play];
+    
+    self.playing = YES;
+    [self configureButtonsWithStatus:self.playing];
+}
+
+- (IBAction)stopRadio:(id)sender
+{
+    [self.player stop];
+    
+    self.playing = NO;
+    [self configureButtonsWithStatus:self.playing];
+}
+
+-(void)configureButtonsWithStatus:(BOOL)status
+{
+    self.playButton.enabled = !self.playing;
+    self.stopButton.enabled = self.playing;
 }
 
 @end
